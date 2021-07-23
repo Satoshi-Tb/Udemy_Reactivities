848c64dd2ed1563059a3be10e17980e246f412be
Satoshi T <sulp91_fy@yahoo.co.jp>
2021-07-23 15:56:54 +0900

    End of Section4

--- API/Startup.cs
+++ API/Startup.cs
@@ -14,6 +14,11 @@ using Microsoft.OpenApi.Models;
 using Domain;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Application.Activities;
+using AutoMapper;
+using Application.Core;
+using API.Extentions;
 
 namespace API
 {
@@ -32,19 +37,7 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            
-            services.AddDbContext<DataContext>(opt => {
-                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt => {
-                opt.AddPolicy("CorsPolicy", policy => {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
+            services.AddApplicationServices(config);
 
         }
 
---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+                public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext context;
+            public Handler(DataContext context)
+            {
+                this.context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var act = await context.Activities.FindAsync(request.Id);
+                this.context.Activities.Remove(act);
+                await context.SaveChangesAsync();
+                return Unit.Value;
+            }
+        }
+    }
+}
